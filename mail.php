f4121b0d430858551756ca9e7d7ef09241b53b27
Ruben Gonzalez <rubengon.uk@gmail.com>
2021-10-16 20:40:13 +0100

    change Form2

--- mail.php
+++ mail.php
@@ -9,11 +9,11 @@ $rugSize= $_POST['rugSize'];
 $to = "rubengon.uk@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\nMessage =" . $message ."\r\n Mobile number =" . $telephoneNo . "\r\n Location =" . $location . "\r\n Rug Size =". $rugSize;
-$headers = "From: RugWebsite@yoursite.com" . "\r\n" .
+$headers = "From: rugWebsite@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:contact.html");
+header("Location:./contact.html");
 ?>
